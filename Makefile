7475c86dfff05f1835d3ac5621e48c9bc2451bc7
Erik Davis <erik@cadlag.org>
2016-07-07 17:52:25 -0700

    moved sphere implementation to sphere.cpp

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=c++
 CFLAGS=-c -std=c++0x -Wall -O3
 LDFLAGS=
 SOURCES=util.cpp aabb.cpp camera.cpp scene.cpp bvh.cpp \
-	material.cpp perlin.cpp raygun.cpp
+	sphere.cpp material.cpp perlin.cpp raygun.cpp
 OBJECTS=$(SOURCES:.cpp=.o)
 EXECUTABLE=raygun
 
---
+++ sphere.cpp
@@ -0,0 +1,85 @@
+#include "sphere.h"
+#include "vec3.h"
+#include "ray.h"
+
+
+bool sphere::hit(const ray& ray, float t_min, float t_max, hit_record &rec) const
+{
+  vec3 oc = ray.origin() - center;
+  float a = dot(ray.direction(), ray.direction());
+  float b = 2.0 * dot(oc, ray.direction());
+  float c = dot(oc, oc) - (radius)*(radius);
+  float discriminant = b*b - 4*a*c;
+  if (discriminant >= 0) {
+    float t = (-b - sqrt(discriminant) ) / (2.0*a);
+    if (t > t_min && t < t_max) {
+      rec.t = t;
+      rec.p = ray.point_at_parameter(t);
+      rec.normal = (rec.p - center)/radius;
+      rec.mat = mat;
+      return true;
+    }
+    t = (-b + sqrt(discriminant) ) / (2.0*a);
+    if (t > t_min && t < t_max) {
+      rec.t = t;
+      rec.p = ray.point_at_parameter(t);
+      rec.normal = (rec.p - center)/radius;
+      rec.mat = mat;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool sphere::bounding_box(float t0, float t1, aabb& box) const
+{
+  box = aabb(center - vec3(radius, radius, radius),
+	     center + vec3(radius, radius, radius));
+  return true;
+}
+
+
+
+vec3 moving_sphere::center(float time) const{
+  return center0 + ((time - time0) / (time1 - time0))*(center1 - center0);
+}
+
+bool moving_sphere::hit(const ray& ray, float t_min, float t_max, hit_record &rec) const
+{
+  vec3 sc = center(ray.time());
+  vec3 oc = ray.origin() - sc;
+  float a = dot(ray.direction(), ray.direction());
+  float b = 2.0 * dot(oc, ray.direction());
+  float c = dot(oc, oc) - (radius)*(radius);
+  float discriminant = b*b - 4*a*c;
+  if (discriminant >= 0) {
+    float t = (-b - sqrt(discriminant) ) / (2.0*a);
+    if (t > t_min && t < t_max) {
+      rec.t = t;
+      rec.p = ray.point_at_parameter(t);
+      rec.normal = (rec.p - sc)/radius;
+      rec.mat = mat;
+      return true;
+    }
+    t = (-b + sqrt(discriminant) ) / (2.0*a);
+    if (t > t_min && t < t_max) {
+      rec.t = t;
+      rec.p = ray.point_at_parameter(t);
+      rec.normal = (rec.p - sc)/radius;
+      rec.mat = mat;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool moving_sphere::bounding_box(float t0, float t1, aabb& box) const {
+
+  vec3 c0 = center(t0);
+  vec3 c1 = center(t1);
+  vec3 rvec = vec3(radius, radius, radius);
+
+  box = surrounding_box(aabb(c0 - rvec, c0 + rvec),
+			aabb(c1 - rvec, c1 + rvec));
+  return true;
+}
